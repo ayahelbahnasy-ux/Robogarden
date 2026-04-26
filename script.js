1bd17f7da3d076d8a6a0352df86924736ee9571a
ayahelbahnasy-ux <ayah.elbahnasy@icloud.com>
2026-04-26 12:15:09 +0300

    Remove HTML tags from script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-<script>
-        $(document).ready(function(){
-            $('.like-btn').click(function(e){
-                e.preventDefault();
-               
-                if($(this).hasClass('like'))
-                {
-                    $(this).removeClass('like');
-                    $(this).addClass('dislike');
-                    $(this).html('Dislike');
-                }
-                else
-                {
-                    $(this).removeClass('dislike');
-                    $(this).addClass('like');
-                    $(this).html('Like');
-                }
-        });
-    </script>
+$(document).ready(function(){
+    $('.like-btn').click(function(e){
+        e.preventDefault();
+       
+        if($(this).hasClass('like'))
+        {
+            $(this).removeClass('like');
+            $(this).addClass('dislike');
+            $(this).html('Dislike');
+        }
+        else
+        {
+            $(this).removeClass('dislike');
+            $(this).addClass('like');
+            $(this).html('Like');
+        }
+    });
+});
